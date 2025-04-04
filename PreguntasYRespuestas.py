a98aac2fbcc0b5ecae002aa97343e2c8154689ad
forlan ordoñez <nalrofor@gmail.com>
2025-04-04 08:46:24 -0500

    agregando 3 y 4 pregunta

--- PreguntasYRespuestas.py
+++ PreguntasYRespuestas.py
@@ -1,16 +1,50 @@
-#sistema de validacion
+# sistema de validacion
 
 print("preguntas y respuestas")
 
 print("""pregunta numero 1
          caul es la capital de colombia?
-         1.barranquilla
-         2.bogota
-         3.santamarta
-         4.medellin      
+         A) barranquilla
+         B) bogota
+         C) santamarta
+         D) medellin      
       """)
 respuesta = input("ingrese la respuesta correcta")
 if respuesta == 2 :
     print("repuesta incorrecta")
 else:
     print("respuesta correcta")
+# esto se debe corregir
+
+
+
+
+# Pregunta 3: Sombrero Vueltiao
+print("¿De dónde es el sombrero vueltiao?")
+print("A) La Guajira")
+print("B) Magdalena")
+print("C) Sucre")
+print("D) Córdoba")  # correcta
+
+respuesta3 = input("Elige una opción (A, B, C o D): ").strip().upper()
+
+# Pregunta 4: Río más largo en Colombia
+print("\n¿Cuál es el río más largo que pasa por Colombia?")
+print("A) Cauca")
+print("B) Sinú")
+print("C) Amazonas")  # Correcta
+print("D) Magdalena")
+
+respuesta4 = input("Elige una opción (A, B, C o D): ").strip().upper()
+
+
+
+# Falta corregir la parte de arriba y falta una pregunta.
+
+# en las preguntas 3 y 4 solo faltaria el if y else ejemplo:
+# if respuesta3 = 'd'
+        # print('correcta es esa')
+# else:
+    # ya sabes que colocar
+
+
