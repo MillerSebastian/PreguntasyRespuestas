bfc6537a83a3159468d16a119820ed3f61fa8dd9
sebastian rodelo <wordwar46@gmail.com>
2025-04-03 19:53:40 -0500

    grabirla: add questions

--- PreguntasYRespuestas.py
+++ PreguntasYRespuestas.py
@@ -1,1 +1,16 @@
-esto es una prueva 
+#sistema de validacion
+
+print("preguntas y respuestas")
+
+print("""pregunta numero 1
+         caul es la capital de colombia?
+         1.barranquilla
+         2.bogota
+         3.santamarta
+         4.medellin      
+      """)
+respuesta = input("ingrese la respuesta correcta")
+if respuesta == 2 :
+    print("repuesta incorrecta")
+else:
+    print("respuesta correcta")
